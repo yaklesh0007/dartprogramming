46231dd59f29086c6ca6d5aacf20a747e596f36d
yaklesh0007 <yaklesh0007@gmail.com>
2022-01-02 15:47:33 +0545

    variable decleration

---
+++ variable.dart
@@ -0,0 +1,14 @@
+void main() {
+  int a = 45;
+  double b = 55;
+  final c = 45.33;
+  String paragraph =
+      """It is a long established fact that a reader will be distracted by the
+readable content of a page when looking at its layout. The point of using Lorem Ipsum is that 
+it has a more-or-less normal distribution of letters, as opposed to using 'Content here, 
+content here', making it look like readable English""";
+  print(paragraph);
+  print(a);
+  print(b);
+  print(c);
+}
