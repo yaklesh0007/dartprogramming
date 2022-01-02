4e081515428807a116b108790e225e47d5a83a74
yaklesh0007 <yaklesh0007@gmail.com>
2022-01-02 16:03:34 +0545

    different way of representing string variable

---
+++ diffstring.dart
@@ -0,0 +1,13 @@
+void main() {
+  String a = 'abc';
+  String b = "ramesh";
+  String para = """ this is adward uncle of queen Elizabeth II.
+  I had adecated that's why she become queen of England.
+  """;
+  String nextway = 'this is adward uncle of queen Elizabeth II. \n'
+      'I had adecated that\' why she become queen of England.\n';
+  print(nextway);
+  print(a);
+  print(b);
+  print(para);
+}
--- variable.dart
+++ variable.dart
@@ -2,6 +2,7 @@ void main() {
   int a = 45;
   double b = 55;
   final c = 45.33;
+  bool rock = false;
   String paragraph =
       """It is a long established fact that a reader will be distracted by the
 readable content of a page when looking at its layout. The point of using Lorem Ipsum is that 
@@ -11,4 +12,5 @@ content here', making it look like readable English""";
   print(a);
   print(b);
   print(c);
+  print(rock);
 }
