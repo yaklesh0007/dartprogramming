c850b2c904f8527f4923c445c27598582cdcfabd
yaklesh0007 <yaklesh0007@gmail.com>
2022-01-03 08:20:59 +0545

    added if else conditrion

---
+++ 3rd.dart
@@ -0,0 +1,10 @@
+void main() {
+  int a = 20;
+  if (a < 50 || a > 20) {
+    print("regular Citezen");
+  } else if (a > 50) {
+    print('senior citizen');
+  } else {
+    print('citizen');
+  }
+}
