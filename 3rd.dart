0dd9091efb8c7363b56bb4cb57f16fc8aa094aa0
yaklesh0007 <yaklesh0007@gmail.com>
2022-01-03 08:22:08 +0545

    && condition

--- 3rd.dart
+++ 3rd.dart
@@ -1,6 +1,9 @@
 void main() {
   int a = 20;
-  if (a < 50 || a > 20) {
+  // if (a < 50 || a > 20) {
+  //   print("regular Citezen");
+  // }
+  if (a < 50 && a > 20) {
     print("regular Citezen");
   } else if (a > 50) {
     print('senior citizen');
