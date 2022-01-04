84c949445933a9eff2094583f224a0516a67763a
yaklesh0007 <yaklesh0007@gmail.com>
2022-01-04 08:59:01 +0545

    key value and enrty

--- mapList.dart
+++ mapList.dart
@@ -4,11 +4,24 @@ void main(List<String> args) {
     'age': 24,
     'job': 'developer'
   };
+  for (final key in person.keys) {
+    print(key);
+  }
+  for (final value in person.values) {
+    print(value);
+  }
+  for (final entry in person.entries) {
+    print(entry);
+  }
+  // print(person['job']);
+  // person['jobs'] = 'fluter';
+  // print(person['jobs']);
 
-  print(person['job']);
-  person['jobs'] = 'fluter';
-  print(person['jobs']);
+  // person.addAll({'name': 'rabin'});
 
-  person.addAll({'name': 'rabin'});
-  print(person);
+  // person.addEntries([
+  //   MapEntry('hobby', 'musician'),
+  //   MapEntry('interst', 'databse Engineering')
+  // ]);
+  // print(person);
 }
